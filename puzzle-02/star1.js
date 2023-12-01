9be927f090050e5501ddfb18acb00fc484971b30
Duppy <Duppy@duppy.com>
2023-12-01 14:38:06 -0800

    👌 IMPROVE: warmed up for Day 02

--- puzzle-02/star1.js
+++ puzzle-02/star1.js
@@ -1,24 +1,35 @@
 /*******************************************************************************
-
-
-*******************************************************************************/
-const readline = require('readline')
+ *  (c) 2023, David 'Duppy' Proctor
+ *  
+ *  PUZZLE-02 STAR 1
+ *  https://adventofcode.com/2020/day/2
+ *******************************************************************************/
+const read_lines = require('readline')
 
 let answer = 0
+let min = Infinity, max = 0
 
 function parse(line) {
+    // Prepare ahead of time with common functions of the line we might need
+    let l = line.length
+    let parts = line.split('')
 
+    // init
+    
+
+    // get to work here
+    return true
 }
 
-async function processLineByLine() {
-    const rl = readline.createInterface({
+async function line_by_line(read_lines) {
+    const rl = read_lines.createInterface({
         input: process.stdin,
         output: process.stdout, // Change output to stdout
         crlfDelay: Infinity
     })
 
     for await (const line of rl) {
-        if (line.length == 0 || !parse(line)) {
+        if (!line || line.length == 0 || !parse(line)) {
             break
         }
     }
@@ -35,5 +46,4 @@ async function processLineByLine() {
         console.log(`${answer} added to clipboard!`)
     })
 }
-
-processLineByLine()
+line_by_line()
