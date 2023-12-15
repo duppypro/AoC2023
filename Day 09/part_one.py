52569d5da55f1c4747f90b4e7d3bf90edabf05a6
Duppy <Duppy@duppy.com>
2023-12-15 00:32:33 -0800

    ⛔sample works, input answer of 1584748276 is too high

--- Day 09/part_one.py
+++ Day 09/part_one.py
@@ -4,27 +4,39 @@
 ####################################################################################################
 
 import sys
+import pyperclip
 
 
 def parse_stdin():
     for line in sys.stdin:
         parse_line(line)
-    print(f'\nEOF')
+    print(f'EOF')
     return
 # end parse_stdin()
 
 def parse_line(line):
+    line = line.strip()
+    numbers = [int(n) for n in line.split(' ')]
+    next_num = next_value(numbers)[-1]
     global total
-    numbers = [int(n) for n in line.split()]
-    total += next_value(numbers)
-    print(line, end='')
+    total += next_num
+    print(f'{line} -> {next_num}\n', end='')
     return
 # end parse()
 
-def next_value(numbers):
-    # calc the difference of last 2 numbers
-    diff = numbers[-1] - numbers[-2]
-    return numbers[-1] + diff
+def next_value(in_numbers: list) -> list:
+    numbers = in_numbers.copy()
+    if numbers[-1] == 0:
+        return numbers  
+    diffs = []
+    sum_of_diffs = 0
+    for i in range(len(numbers) - 1):
+        diffs.append(numbers[i + 1] - numbers[i])
+        sum_of_diffs += diffs[-1]
+    # print(f'    diffs = {diffs}')
+    next_num = next_value(diffs).copy()
+    numbers.append(numbers[-1] + next_num[-1])
+    return numbers.copy()
 # end next_value()
 
 
@@ -33,3 +45,7 @@ if __name__ == '__main__':
     parse_stdin()
     print(f'\n    sample.txt expects 18 + 28 + 68 = 114')
     print(f'    total = {total}')
+    # add total to the system clipboard
+    pyperclip.copy(total)
+    print(f'    {total} copied to clipboard')
+    
