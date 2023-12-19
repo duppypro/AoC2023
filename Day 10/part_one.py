89cb565c325aed3a837dfa2a202d6a3b98bf2474
duppy <duppy@duppy.com>
2023-12-19 04:13:24 -0800

    🤞Day 10 part_one.py passes samples

--- Day 10/part_one.py
+++ Day 10/part_one.py
@@ -8,10 +8,33 @@
 #
 ####################################################################################################
 
+import dis
+from os import pipe
+from sqlite3 import connect
 import sys
+from turtle import right
+import pip
 import pyperclip
 import argparse
 
+pipe_2d = []
+dist_2d = []
+criter_location = [-1, -1]
+
+def print_2d():
+    loc = [0,0]
+    for row in pipe_2d:
+        loc[1] = 0
+        for char in row:
+            dist = int(peek_2d(dist_2d, loc))
+            if dist > 0:
+                print(f'{dist % 10}', end='')
+            else:
+                print(char, end='')
+            loc[1] += 1
+        print()
+        loc[0] += 1
+    
 def begin(answer=0):
     answer = 0
     print('BEGIN')
@@ -20,19 +43,91 @@ def begin(answer=0):
 def parse_file(input, answer):
     with open(input, 'r') as sys.stdin:
         for line in sys.stdin:
-            answer = parse_line(line, answer)
+            row = []
+            answer = parse_line(line, row, answer)
+            pipe_2d.append(row)
+            dist_2d.append([-1] * len(row))
     print(f'EOF: file closed')
     return answer
 
+def clip(x, _min, _max):
+    return max(_min, min(_max, x))
+
+def move_2d(start, delta):
+    row = clip(start[0] + delta[0], 0, len(pipe_2d) - 1)
+    col = clip(start[1] + delta[1], 0, len(pipe_2d[row]) - 1)
+    return [row, col]
+
+def peek_2d(array_2d, location):
+    row = location[0]
+    col = location[1]
+    if row < 0 or row >= len(array_2d):
+        return ' '
+    if col < 0 or col >= len(array_2d[0]):
+        return ' '  
+    return array_2d[row][col]
+
+def poke_2d(array_2d, location, value):
+    row = location[0]
+    col = location[1]
+    if row < 0 or row >= len(array_2d):
+        return
+    if col < 0 or col >= len(array_2d[0]):
+        return  
+    array_2d[location[0]][location[1]] = value
+
 def end(answer):
-    print('END')
+    print('\nEND')
+    print_2d()
+    left_fill = criter_location.copy() # don't really need to copy in this case
+    right_fill = criter_location.copy()
+    num_steps = -1
+    while peek_2d(dist_2d, left_fill) == -1:
+        num_steps += 1
+        poke_2d(dist_2d, left_fill, num_steps)
+        poke_2d(dist_2d, right_fill, num_steps)
+        # brute force (unroll) search for connected pipes
+        cw         = [[0, -1], [-1, 0], [0, 1], [1, 0]]
+        connect_cw = [  '┌└─',   '┌┐│',  '┐┘─',  '┘└│']
+        old_right_fill = right_fill.copy()
+        for i, look_dir in enumerate(cw):
+            look_at = move_2d(right_fill, look_dir)
+            if peek_2d(pipe_2d, look_at) in connect_cw[i]:
+                if peek_2d(pipe_2d, right_fill) in (connect_cw[(i+2) % 4] + 'X'):
+                    right_fill = look_at.copy()
+                    break
+        ccw         = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+        connect_ccw = [ '┘└│',  '┐┘─',   '┌┐│',   '┌└─']
+        old_left_fill = left_fill.copy()
+        for i, look_dir in enumerate(ccw):
+            look_at = move_2d(left_fill, look_dir)
+            if peek_2d(pipe_2d, look_at) in connect_ccw[i]:
+                if peek_2d(pipe_2d, left_fill) in (connect_ccw[(i+2) % 4] + 'X'):
+                    left_fill = look_at.copy()
+                    break
+        poke_2d(pipe_2d, old_left_fill, f'{num_steps % 10}')
+        poke_2d(pipe_2d, old_right_fill, f'{num_steps % 10}')
+        print(f'\nnum_steps = {num_steps}')
+        print_2d()
+        
+        
+    return num_steps
+
+def parse_line(line, row, answer):
+    line = line.strip()
+    for char in line:
+        pretty = map_prettier(char)
+        if pretty == 'X':
+            criter_location[0] = len(pipe_2d)
+            criter_location[1] = len(row)
+        row.append(pretty)
     return answer
 
-def parse_line(line, answer):
-    answer += 1
-    line = line.strip()
-    print(f'{line}\n', end='')
-    return answer
+def map_prettier(char):
+    c = 'F7JL-|.S'
+    p = '┌┐┘└─│ X'
+    to_prettier = dict(zip(c, p))
+    return to_prettier[char]
 
 if __name__ == '__main__':
     OG_stdout = sys.stdout
